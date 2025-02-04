cb175c5ef20612a5bf70ceafd2344df7fff5169f
Tam Le <br4nch@Tams-MacBook-Pro.local>
2025-02-04 16:49:05 +0700

    Implement next-auth

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from "next";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className={"space-y-4"}>
+          <Link href="/" className={"flex-center"}>
+            <Image
+              src={"/images/logo.svg"}
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className={"text-center"}>Sign In</CardTitle>
+          <CardDescription className={"text-center"}>
+            Sign in to your account
+          </CardDescription>
+          <CardContent className={"space-y-4"}>{/*  FORM HERE */}</CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
