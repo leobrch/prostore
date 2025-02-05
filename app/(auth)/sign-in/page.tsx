2e49783436e1fd8e3f6b090ceced833f42e122dd
Tam Le <br4nch@Tams-MacBook-Pro.local>
2025-02-05 17:01:00 +0700

    Implement sign-in

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,12 +9,25 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
+import CredentialsSignInForm from "@/app/(auth)/sign-in/credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign in",
 };
 
-const SignInPage = () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
@@ -32,7 +45,9 @@ const SignInPage = () => {
           <CardDescription className={"text-center"}>
             Sign in to your account
           </CardDescription>
-          <CardContent className={"space-y-4"}>{/*  FORM HERE */}</CardContent>
+          <CardContent className={"space-y-4"}>
+            <CredentialsSignInForm />
+          </CardContent>
         </CardHeader>
       </Card>
     </div>
